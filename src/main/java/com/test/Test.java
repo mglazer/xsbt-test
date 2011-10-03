e16c3b13b8766ac64103ded382fbb2e0e448e5cf
Mike Glazer <mglazer@palantir.com>
2011-10-02 21:51:00 -0700

    Initial commit

---
+++ src/main/java/com/test/Test.java
@@ -0,0 +1,12 @@
+package com.test;
+
+import org.springframework.util.StringUtils;
+
+class Test {
+
+	public static void main(String[] args) {
+		System.out.println(StringUtils.isBlank(" hello world ") );
+
+	}
+
+}
